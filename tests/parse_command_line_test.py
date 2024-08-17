918b2560d2355362f02c2dacdc07fa6061909a3e
mohammadjarabah681 <mohammadjarabah681@users.noreply.github.com>
2024-08-17 18:59:50 +0300

    refactor 'tests' directory.

--- tests/parse_command_line_test.py
+++ tests/parse_command_line_test.py
@@ -1,5 +1,12 @@
 from utils.parse_command_line import parse_command_line
 
 def test_parse_command_line():
-    assert parse_command_line("ls ./src -al") == { "command": "ls", "arguments": ["./src", "-al"] }
-    assert parse_command_line("py -m venv .venv") == {"command": "py", "arguments": ["-m", "venv", ".venv"]}
+    assert parse_command_line("ls ./src -al") == {
+        "command": "ls",
+        "arguments": ["./src", "-al"]
+    }
+
+    assert parse_command_line("py -m venv .venv") == {
+        "command": "py",
+        "arguments": ["-m", "venv", ".venv"]
+    }
